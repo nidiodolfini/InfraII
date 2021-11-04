f7b2105aa1c72be0cfa544d0209b1f3e3652fa23
Nídio Dolfini <nidiosdolfini@gmail.com>
2021-11-04 13:31:28 -0300

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.13.0"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
