d00c972d355b22feecfe6473f89a4a6d90816f1a
Nidio Dolfini <nidiosdolfini@gmail.com>
2021-11-17 09:00:19 -0300

    banana alteração

--- dockerspring/src/main/java/nidio/dev/dockerspring/controller/HelloController.java
+++ dockerspring/src/main/java/nidio/dev/dockerspring/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String helloMessage(){
-        return "Hello World";
+        return "Hello World2";
     }
 }
